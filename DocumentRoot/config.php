e5e397814898c15d069ea76aa11038e3f578a3d5
Steve Wilson <sew.credulous@gmail.com>
2022-08-21 10:29:53 +0100

    Fixed issue with DB connect .. wrong database host!! moved authenticate.php to login.php .. sounds better

--- DocumentRoot/config.php
+++ DocumentRoot/config.php
@@ -1,4 +1,5 @@
-$DATABASE_HOST = 'groucho.credulous.home';
+<?php
+$DATABASE_HOST = 'mariadb';
 $DATABASE_USER = '_svc_sm';
 $DATABASE_PASS = 'smpass';
 $DATABASE_NAME = 'safe_mgmt';
@@ -8,4 +9,5 @@ if ( mysqli_connect_errno() ) {
         // If there is an error with the connection, stop the script and display the error.
         exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-
+echo '<h1>Encryption Services Key Inventory</h1>';
+?>

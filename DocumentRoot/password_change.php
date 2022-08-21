3bc1b7cb2456e409cd92430018d9ab96ce1583aa
Steve Wilson <sew.credulous@gmail.com>
2022-08-21 13:44:25 +0100

    Password expiry, now need to work on password change screen

---
+++ DocumentRoot/password_change.php
@@ -0,0 +1,34 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Password_change</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Encryption Services safe management</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Password change</h2>
+			<p>Welcome back, <?=$_SESSION['name']?> your password has expired, please change</p>
+			<p>Password expiry: <?=$_SESSION['password_expiry']?></p>
+			<p>Now is: <?=$_SESSION['now']?></p>
+			<a href="password.php">Password</a>
+		</div>
+	</body>
+</html>

8495bb7bac2c27549f38c4b96b4ef4d78214bb43
Steve Wilson <sew.credulous@gmail.com>
2022-08-23 05:07:22 +0100

    added passwdqc-utils so I can use pwqgen. adde demo script

--- DocumentRoot/password_generation_script.php
+++ DocumentRoot/password_generation_script.php
@@ -6,12 +6,18 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
 }
+
+$command = "/usr/bin/pwqgen";
+exec($command, $out, $ret);
+if ( $ret == 0 ) { 
+	$passphrase = $out[0]; 
+}
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Main menu</title>
+		<title>Password generation</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
@@ -19,14 +25,15 @@ if (!isset($_SESSION['loggedin'])) {
 		<nav class="navtop">
 			<div>
 				<h1>Encryption Services safe management</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="main_menu.php"><i class="fas fa-map-signs"></i>Main menu</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 		<div class="content">
-			<h2>Main menu</h2>
+			<h2>Password generation</h2>
 			<p>Welcome back, <?=$_SESSION['name']?>!</p>
-			<a href="password_generation_script.php">Password generation</a>
+			<p>Passphrase is: 
+			<mark><em><b><?=$passphrase?></b></em></mark></p>
 		</div>
 	</body>
 </html>

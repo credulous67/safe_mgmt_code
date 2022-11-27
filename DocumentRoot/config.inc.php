8a4dee0d9ba7d0cc508d4dd5c9e0993316d56ff2
Steve Wilson <sew.credulous@gmail.com>
2022-11-27 17:05:42 +0000

    remove ?> from end of include php files

--- DocumentRoot/config.inc.php
+++ DocumentRoot/config.inc.php
@@ -10,4 +10,3 @@ if ( mysqli_connect_errno() ) {
         // If there is an error with the connection, stop the script and display the error.
         exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-?>
